fd4e1f16487eef66976671109892496b659af223
GiustiRodriguez <giusti.rodriguezc@pca.edu.co>
2020-09-27 16:11:42 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -17,14 +17,6 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 // Our web handlers
 
-$app->get('/{dato}', function($dato) use($app){
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig', array(
-      'dato' => $dato,
-      ));
-)};
-
-
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
